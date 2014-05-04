0ea4754acfa9a482fbb86dcc2a90f8f402d457a9
Gabriel Calvo <gabrielcalvo.calvogarcia@gmail.com>
2014-05-04 16:48:07 +0200

    Fase Inicial

---
+++ app/controllers/Application.java
@@ -0,0 +1,34 @@
+package controllers;
+
+import java.io.File;
+
+import play.mvc.Controller;
+import play.mvc.Http.MultipartFormData;
+import play.mvc.Http.MultipartFormData.FilePart;
+import play.mvc.Result;
+import views.html.*;
+
+public class Application extends Controller {
+
+    public static Result index() {
+        return ok(index.render("¿Subida de archivos?"));
+    }
+    
+    public static Result showUpload() {
+    	return ok(upload.render());
+    }
+    
+    public static Result uploadFile() {
+    	MultipartFormData body = request().body().asMultipartFormData();
+    	FilePart file = body.getFile("file");
+    	if (file != null) {
+    		String fileName = file.getFilename();
+    		String contentType = file.getContentType();
+    		File archivo = file.getFile();
+    		return ok(index.render("Archivo Subido!"));
+    	}
+    	
+    	return internalServerError("Error en la subida de archivo");
+    }
+
+}
